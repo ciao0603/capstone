9119e3bbe039cb445425ce82b39acc67a203ab34
ciao0603 <jocelyn94032.0@gmail.com>
2023-10-01 16:34:03 +0800

    feat: add User model

---
+++ migrations/20231001082856-create-user.js
@@ -0,0 +1,50 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      is_teacher: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      introduction: {
+        type: Sequelize.STRING
+      },
+      total_hours: {
+        type: Sequelize.INTEGER
+      },
+      nation: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
+  }
+}
