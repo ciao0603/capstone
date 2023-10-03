7616f5d8ce0a5255339f58219ff0cc474b560569
ciao0603 <jocelyn94032.0@gmail.com>
2023-10-03 16:27:58 +0800

    feat: add Tutor model

---
+++ migrations/20231003081806-create-tutor.js
@@ -0,0 +1,45 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tutors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      introduction: {
+        type: Sequelize.TEXT
+      },
+      style: {
+        type: Sequelize.TEXT
+      },
+      link: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      available_days: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tutors')
+  }
+}
---
+++ models/tutor.js
@@ -0,0 +1,31 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Tutor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  Tutor.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    style: DataTypes.TEXT,
+    link: DataTypes.STRING,
+    duration: DataTypes.INTEGER,
+    availableDays: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Tutor',
+    tableName: 'Tutors',
+    underscored: true
+  })
+  return Tutor
+}
