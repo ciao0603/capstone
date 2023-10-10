c2ae9e966991965c5b3e9767b7d367e729fca733
ciao0603 <jocelyn94032.0@gmail.com>
2023-10-10 13:17:21 +0800

    feat: Modify courseController to avoid repeated reserve

--- controllers/course-controller.js
+++ controllers/course-controller.js
@@ -1,32 +1,50 @@
 const dayjs = require('dayjs')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+
 const { Op } = require('sequelize')
 // 設置dayjs為中文
 require('dayjs/locale/zh-cn')
 dayjs.locale('zh-cn')
+dayjs.extend(customParseFormat)
 
 const { Tutor, Course } = require('../models')
 
 const courseController = {
-  getCourse: (req, res, next) => {
-    const { id } = req.params
-    const times = []
-    const today = dayjs()
-    Tutor.findByPk(id, { raw: true })
-      .then(tutor => {
-        if (!tutor) throw new Error('這位家教不存在!')
+  getCourse: async (req, res, next) => {
+    try {
+      const tutorId = req.params.id
+      const times = []
+      const today = dayjs()
 
-        const { duration, availableDays } = tutor
-        const courseCount = 180 / duration // 每個晚上開課的次數
-        // 從當天開始加14天
-        for (let d = 0; d < 14; d++) {
-          const date = today.add(d, 'day')
-          const dayOfWeek = date.day().toString()
-          // 若是date的星期和tutor可以上課的星期一樣
-          if (availableDays.includes(dayOfWeek)) {
-            let startTime = date.set('hour', 18).set('minute', 0)
-            // 根據duration產生當天可上課的時段
-            for (let i = 0; i < courseCount; i++) {
-              const endTime = startTime.add(duration, 'minute')
+      const tutor = await Tutor.findByPk(tutorId, { raw: true })
+      if (!tutor) throw new Error('這位家教不存在!')
+      const { duration, availableDays } = tutor
+      const courseCount = 180 / duration // 每個晚上開課的次數
+
+      // 從當天開始加14天
+      for (let d = 0; d < 14; d++) {
+        const date = today.add(d, 'day')
+        const dayOfWeek = date.day().toString()
+        // 若是date的星期和tutor可以上課的星期一樣
+        if (availableDays.includes(dayOfWeek)) {
+          let startTime = date.set('hour', 18).set('minute', 0)
+          // 根據duration產生當天可上課的時段
+          for (let i = 0; i < courseCount; i++) {
+            const endTime = startTime.add(duration, 'minute')
+            const formattedDate = new Date(date.format('YYYY-MM-DD'))
+            const formattedStartTime = startTime.format('HH:mm:00')
+            const courses = await Course.findAll()
+            console.log(courses[0])
+            // 確認時段是否已被預約
+            const course = await Course.findOne({
+              where: {
+                [Op.and]: [{ tutorId }, { date: formattedDate }, {
+                  startTime: formattedStartTime
+                }]
+              }
+            })
+            console.log(course)
+            if (!course) {
               // 儲存這個課程時段
               times.push({
                 date: date.format('YYYY-MM-DD'),
@@ -34,14 +52,16 @@ const courseController = {
                 startTime: startTime.format('HH:mm'),
                 endTime: endTime.format('HH:mm')
               })
-              // 重置下堂課開始時間
-              startTime = endTime
             }
+            // 重置下堂課開始時間
+            startTime = endTime
           }
         }
-        res.render('course', { tutor, times })
-      })
-      .catch(err => next(err))
+      }
+      res.render('course', { tutor, times })
+    } catch (err) {
+      next(err)
+    }
   },
   postCourse: (req, res, next) => {
     const userId = req.user.id
@@ -49,32 +69,36 @@ const courseController = {
     // 取得預約時間並轉成json
     const time = JSON.parse(req.body.time)
     const { date, dayOfWeek, startTime, endTime } = time
+    console.log(date)
+    const formattedDate = new Date(date)
+    const formattedStartTime = `${startTime}:00`
+
+    console.log(formattedDate)
 
     Course.findOne({
       // where 同老師 、 同一天 且 同時間
-      where: { [Op.and]: [{ tutorId }, { date }, { startTime }] },
-      include: [Tutor]
+      where: { [Op.and]: [{ tutorId }, { date: formattedDate }, { startTime: formattedStartTime }] }
     })
       .then(course => {
-        if (course) return
-        console.log('create')
-        return Course.create({
-          date,
-          dayOfWeek,
-          startTime,
-          endTime,
-          userId,
-          tutorId
-        })
+        if (!course) {
+          return Course.create({
+            date,
+            dayOfWeek,
+            startTime,
+            endTime,
+            userId,
+            tutorId
+          })
+        }
       })
       .then(newCourse => {
-        if (!newCourse) return
-        console.log('find')
-        return Course.findByPk(newCourse.id, {
-          raw: true,
-          nest: true,
-          include: [Tutor]
-        })
+        if (newCourse) {
+          return Course.findByPk(newCourse.id, {
+            raw: true,
+            nest: true,
+            include: [Tutor]
+          })
+        }
       })
       .then(reserve => {
         if (reserve) {
